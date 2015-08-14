a3478b4dfef1a01a922bbf777ee19ee0ebde8099
Dan Hopkins <dphopkins94@gmail.com>
2015-08-13 20:05:46 -0400

    Functional, just needs a few UI improvements, which are documented in Issues

--- BaseballBot.py
+++ BaseballBot.py
@@ -1,13 +1,15 @@
 import telebot
+from telebot import types
 from datetime import datetime, timedelta
 import requests
 import json
 
-# THANKS TO fspinillo, casschin, and jonursenbach ON GITHUB FOR THE CODE, WHICH I HAVE EDITED FOR MY PURPOSES
+# uses python-baseball by fspinillo, casschin, and jonursenbach on GitHub: https://github.com/fspinillo/python-baseball
 
 API_TOKEN = '59094683:AAG5mNV5wOSTFq8VjfdU0OlDETvuHMXdgBM'
 
-bot = telebot.TeleBot(API_TOKEN)
+#bot = telebot.TeleBot(API_TOKEN)
+bot = telebot.TeleBot(API_TOKEN, True, 4)
 
 diamondbacks = ["Arizona", "Diamondbacks", "ARI"]
 braves = ["Atlanta", "Braves", "ATL"]
@@ -55,112 +57,101 @@ for i in range(0, len(teams)):
 
 alts = ["CHICAGO", "LOS ANGELES", "NEW YORK"]
 
-"""
-AL_East = [orioles, red_sox, yankees, rays, blue_jays]
-NL_East = [braves, marlins, mets, phillies, nationals]
-AL_Central = [white_sox, indians, tigers, royals, twins]
-NL_Central = [cubs, reds, brewers, pirates, cardinals]
-AL_West = [astros, angels, athletics, mariners, rangers]
-NL_West = [diamondbacks, rockies, dodgers, padres, giants]
-"""
+divisions = ["AL WEST", "AL CENTRAL", "AL EAST", "NL WEST", "NL CENTRAL", "NL EAST"]
 
-#function to determine the status of a game, if no team selected
+# determines the status of a game, if no team selected
 def game_info(game):
     if game['status']['status'] == "In Progress":
-        return '%s (%s) vs %s (%s) @ %s %s' % (
-                game['away_team_name'],
-                game['linescore']['r']['away'],
-                game['home_team_name'],
-                game['linescore']['r']['home'],
-                game['venue'],
-                game['status']['status']
-            )
+        return 'In Progress: %s (%s) @ %s (%s)' % (
+            game['away_team_name'],
+            game['linescore']['r']['away'],
+            game['home_team_name'],
+            game['linescore']['r']['home']
+        )
     elif (game['status']['status'] == "Final" or game['status']['status'] == "Game Over"):
-        return '%s (%s) vs %s (%s) @ %s %s' % (
-                game['away_team_name'],
-                game['linescore']['r']['away'],
-                game['home_team_name'],
-                game['linescore']['r']['home'],
-                game['venue'],
-                game['status']['status']
-            )
+        return 'Final: %s (%s) @ %s (%s)' % (
+            game['away_team_name'],
+            game['linescore']['r']['away'],
+            game['home_team_name'],
+            game['linescore']['r']['home']           
+        )
     elif (game['status']['status'] == "Pre-Game" or game['status']['status'] == "Preview"):
-        return '%s vs %s @ %s %s%s %s' % (
-                game['away_team_name'],
-                game['home_team_name'],
-                game['venue'],
-                game['home_time'],
-                game['hm_lg_ampm'],
-                game['status']['status']
-            )
+        return \
+        'Pre-Game: %s @ %s\n' \
+        '%s%s at %s' % (
+            game['away_team_name'],
+            game['home_team_name'],
+            game['home_time'],
+            game['hm_lg_ampm'],
+            game['venue']
+        )
 
-#function to determine the status of a game, if a team is selected
+# determines the status of a game if a team is selected
 def team_score(game):
     if game['status']['status'] == "In Progress":
+        if game['status']['inning'] == "1":
+            suffix = "st"
+        elif game['status']['inning'] == "2":
+            suffix = "nd"
+        elif game['status']['inning'] == "3":
+            suffix = "rd"
+        else:
+            suffix = "th"
+
         return \
-        '-------------------------------\n' \
-        '%s (%s) vs. %s (%s) @ %s\n' \
-        '%s: %s of the %s\n' \
-        'Pitching: %s || Batting: %s || S: %s B: %s O: %s\n' \
-        '-------------------------------' % (
-                game['away_team_name'],
-                game['linescore']['r']['away'],
-                game['home_team_name'],
-                game['linescore']['r']['home'],
-                game['venue'],
-                game['status']['status'],
-                game['status']['inning_state'],
-                game['status']['inning'],
-                game['pitcher']['last'],
-                game['batter']['last'],
-                game['status']['s'],
-                game['status']['b'],
-                game['status']['o']
-            )
+        '%s (%s) @ %s (%s)\n' \
+        'In Progress: %s of the %s%s\n' \
+        'Pitching: %s || Batting: %s\n' \
+        'S: %s || B: %s || O: %s' % (
+            game['away_team_name'],
+            game['linescore']['r']['away'],
+            game['home_team_name'],
+            game['linescore']['r']['home'],
+            game['status']['inning_state'],
+            game['status']['inning'],
+            suffix,
+            game['pitcher']['last'],
+            game['batter']['last'],
+            game['status']['s'],
+            game['status']['b'],
+            game['status']['o']
+        )
     elif (game['status']['status'] == "Final" or game['status']['status'] == "Game Over"):
         return \
-        '-------------------------------\n' \
-        '%s (%s) vs. %s (%s) @ %s\n' \
-        'W: %s || L: %s || SV: %s\n' \
-        '-------------------------------' % (
-                game['away_team_name'],
-                game['linescore']['r']['away'],
-                game['home_team_name'],
-                game['linescore']['r']['home'],
-                game['venue'],
-                game['winning_pitcher']['name_display_roster'],
-                game['losing_pitcher']['name_display_roster'],
-                game['save_pitcher']['name_display_roster']
-            )
+        'Final: %s (%s) @ %s (%s)\n' \
+        'W: %s || L: %s || SV: %s' % (
+            game['away_team_name'],
+            game['linescore']['r']['away'],
+            game['home_team_name'],
+            game['linescore']['r']['home'],
+            game['winning_pitcher']['name_display_roster'],
+            game['losing_pitcher']['name_display_roster'],
+            game['save_pitcher']['name_display_roster']
+        )
     elif (game['status']['status'] == "Pre-Game" or game['status']['status'] == "Preview"):
         return \
-        '-------------------------------\n' \
-        '%s vs %s @ %s %s%s\n' \
-        'P: %s || P: %s\n' \
-        '-------------------------------' % (
-                game['away_team_name'],
-                game['home_team_name'],
-                game['venue'],
-                game['home_time'],
-                game['hm_lg_ampm'],
-                game['away_probable_pitcher']['name_display_roster'],
-                game['home_probable_pitcher']['name_display_roster']
-               )
+        'Pregame: %s @ %s\n' \
+        '%s%s at %s\n' \
+        'P: %s vs %s' % (
+            game['away_team_name'],
+            game['home_team_name'],
+            game['home_time'],
+            game['hm_lg_ampm'],
+            game['venue'],
+            game['away_probable_pitcher']['name_display_roster'],
+            game['home_probable_pitcher']['name_display_roster']
+        )
 
-#function to determine which feed to grab based on user input
+# determines which feed to grab based on user input
 def date_url(date):
-    if date == "yesterday":
-        baseball_url = "http://gd2.mlb.com/components/game/mlb/year_%d/month_%s/day_%s/master_scoreboard.json" \
-        % (now.year, now.strftime("%m"), yesterday.strftime("%d"))
-    else:
-        baseball_url = "http://gd2.mlb.com/components/game/mlb/year_%d/month_%s/day_%s/master_scoreboard.json" \
-                % (now.year, now.strftime("%m"), now.strftime("%d"))
+    baseball_url = "http://gd2.mlb.com/components/game/mlb/year_%d/month_%s/day_%s/master_scoreboard.json" % (now.year, now.strftime("%m"), now.strftime("%d"))
+    # for last now.strftime("%d"), use yesterday or day_before instead of now to see games on other days, depending on what date ==
     return baseball_url
 
 # Set date
 now = datetime.now()
-yesterday = datetime.now() - timedelta(days=1)
-day_before = datetime.now() - timedelta(days=2)
+# yesterday = datetime.now() - timedelta(days=1)
+# day_before = datetime.now() - timedelta(days=2)
 
 # builds data structure from feed
 baseball_data = requests.get(date_url("today")).json()
@@ -171,22 +162,60 @@ game_array = baseball_data['data']['games']['game']
 def send_welcome(message):
     bot.reply_to(message, """\
 Hi there, I am BaseballBot!
-Use /current to see all current games. You can request to see the score of the most recent game for a specific team by typing in the city, team name, or abbreviation (like PHI for the Phillies).\
+Use /all to see all of today's games or request the score of today's game for a specific team by typing in the city, team name, or abbreviation.\
 """)
 
-# Handle '/clean'
-@bot.message_handler(commands=['current'])
+# Handle '/all'
+@bot.message_handler(commands=['all'])
 def respond(message):
+    chat_id = message.chat.id
     for game in game_array:
         disp = game_info(game)
         if disp:
-            bot.reply_to(message, disp) # currently sends a message per game
+            bot.send_message(chat_id, disp) # currently sends one message per game
+
+# Handle '/division'
+@bot.message_handler(commands=['division'])
+def respond(message):
+    chat_id = message.chat.id
+    division_markup = types.ReplyKeyboardMarkup()
+    division_markup.row('AL West', 'AL Central', 'AL East')
+    division_markup.row('NL West', 'NL Central', 'NL East')
+    bot.send_message(chat_id, "Select which division you'd like to check out:", reply_markup=division_markup)
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def respond(message):
+    chat_id = message.chat.id
     txt = message.text.upper()
-    if txt in dictionary.keys():
+
+    if txt in divisions: # TODO: use a loop instead
+        if txt == "AL WEST":
+            al_west_markup = types.ReplyKeyboardMarkup()
+            al_west_markup.add('HOU', 'LAA', 'OAK', 'SEA', 'TEX')
+            bot.send_message(chat_id, "Select the team you want to see:", reply_markup=al_west_markup)
+        elif txt == "AL CENTRAL":
+            al_central_markup = types.ReplyKeyboardMarkup()
+            al_central_markup.add('CWS', 'CLE', 'DET', 'KC', 'MIN')
+            bot.send_message(chat_id, "Select the team you want to see:", reply_markup=al_central_markup)
+        elif txt == "AL EAST":
+            al_east_markup = types.ReplyKeyboardMarkup()
+            al_east_markup.add('BAL', 'BOS', 'NYY', 'TB', 'TOR')
+            bot.send_message(chat_id, "Select the team you want to see:", reply_markup=al_east_markup)
+        elif txt == "NL WEST":
+            nl_west_markup = types.ReplyKeyboardMarkup()
+            nl_west_markup.add('ARI', 'COL', 'LAD', 'PD', 'SF')
+            bot.send_message(chat_id, "Select the team you want to see:", reply_markup=nl_west_markup)
+        elif txt == "NL CENTRAL":
+            nl_central_markup = types.ReplyKeyboardMarkup()
+            nl_central_markup.add('CHC', 'CIN', 'MIL', 'PIT', 'STL')
+            bot.send_message(chat_id, "Select the team you want to see:", reply_markup=nl_central_markup)
+        elif txt == "NL EAST":
+            nl_east_markup = types.ReplyKeyboardMarkup()
+            nl_east_markup.add('ATL', 'MIA', 'NYM', 'PHI', 'WSH')
+            bot.send_message(chat_id, "Select the team you want to see:", reply_markup=nl_east_markup)
+
+    elif txt in dictionary.keys():
         if txt in alts:
             if txt == "CHICAGO":
                 bot.reply_to(message, "Cubs or White Sox?")
@@ -201,14 +230,18 @@ def respond(message):
                 if (game['home_name_abbrev']) == response or (game['away_name_abbrev']) == response:
                     disp = team_score(game)
                     if disp:
-                        bot.reply_to(message, disp)
+                        bot.send_message(chat_id, disp)
                     found = True
             if found == False:
-                bot.reply_to(message, "No games today")
+                bot.send_message(chat_id, "No games for that team today, sorry!")
     else:
-        bot.reply_to(message, "There was an error. Check your spelling and try again.")
+        bot.send_message(chat_id, "There was an error. Check your spelling and try again.")
 
 bot.polling()
 
 while True:
     pass
+
+# /all - see all of today's games
+# /division - search for a team by division
+# /help - get more info
